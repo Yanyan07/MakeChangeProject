18b1b448e93157f0c7a20333ad8b805de90514f6
Yanyan07 <yenj2015@gmail.com>
2021-07-24 11:22:21 -0600

    create make change project

--- README.md
+++ README.md
@@ -2,16 +2,20 @@
 
 ### Overview
 
-Explain the basic idea of the project
+Make a cash register to calculate change returned to a customer
 
 ### How to Use
 
-Provide the user instructions on how to run your project
+Enter item price and the amount of money tendered, this system will calculate the amount of change and show how many  of each piece of currency is need.
 
 ### Technologies
 
 * Conditionals
+* Method
 * Modulus Operator
+* While loop
+* Data Types
 
 ### Lessons Learned
-Main takeaways from work on the project
+
+Double numbers are stored in memory differently from integers. 
---
+++ src/MakeChange.java
@@ -0,0 +1,120 @@
+import java.util.Scanner;
+
+public class MakeChange {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		double price = 0.0;
+		double pay = 0.0;
+		double change = 0.0;
+		String isExit = "";
+
+		while (true) {
+			welcome();
+			isExit = sc.next();
+			System.out.println();
+			if ("q".equals(isExit) || "Q".equals(isExit)) {
+				System.out.println("Exit from the make change system, welcome to come again!");
+				break;
+			}
+			System.out.print("What is the price of the item? ");
+			price = sc.nextDouble();
+			System.out.print("How much money was tendered by the customer? ");
+			pay = sc.nextDouble();
+
+			if (price > pay) {
+				System.out.println("The money tendered is not adequate, can't make change for you!");
+				continue;
+			}else if(price == pay) {
+				System.out.println("The money tendered is exact the same as the item price, perfect!");
+				continue;
+			}
+			System.out.println();
+			change = pay - price + 0.0001;
+			printChange(change);
+		}
+
+		sc.close();
+	}
+
+	public static void welcome() {
+		System.out.println("--------------------------------------------");
+		System.out.println("$    Welcome to make change system!      $");
+		System.out.println("--------------------------------------------");
+		System.out.print("     Enter y/Y to continue, q/Q to exit:   ");
+	}
+
+	public static void printChange(double change) {
+		int twentyCount = 0;
+		int tenCount = 0;
+		int fiveCount = 0;
+		int oneCount = 0;
+		int quarterCount = 0;
+		int dimeCount = 0;
+		int nickelCount = 0;
+		int pennyCount = 0;
+
+		if (change >= 20) {
+			twentyCount = (int) (change / 20);
+			change %= 20;
+		}
+		if (change >= 10) {
+			tenCount = (int) (change / 10);
+			change %= 10;
+		}
+		if (change >= 5) {
+			fiveCount = (int) (change / 5);
+			change %= 5;
+		}
+		if (change >= 1) {
+			oneCount = (int) (change / 1);
+			change %= 1;
+		}
+		if (change >= 0.25) {
+			quarterCount = (int) (change / 0.25);
+			change %= 0.25;
+		}
+		if (change >= 0.1) {
+			dimeCount = (int) (change / 0.1);
+			change %= 0.1;
+		}
+		if (change >= 0.05) {
+			nickelCount = (int) (change / 0.05);
+			change %= 0.05;
+		}
+		if (change >= 0.01) {
+			pennyCount = (int) Math.round(change / 0.01);
+			change %= 0.01;
+		}
+
+		System.out.print("Change: ");
+		if (twentyCount != 0) {
+			System.out.print(twentyCount + " twenty dollar bill ");
+		}
+		if (tenCount != 0) {
+			System.out.print(tenCount + " ten dollar bill ");
+		}
+		if (fiveCount != 0) {
+			System.out.print(fiveCount + " five dollar bill ");
+		}
+		if (oneCount != 0) {
+			System.out.print(oneCount + " one dollar bill ");
+		}
+		if (quarterCount != 0) {
+			System.out.print(quarterCount + " quarter ");
+		}
+		if (dimeCount != 0) {
+			System.out.print(dimeCount + " dime ");
+		}
+		if (nickelCount != 0) {
+			System.out.print(nickelCount + " nickel ");
+		}
+		if (pennyCount != 0) {
+			System.out.print(pennyCount + " penny ");
+		}
+		System.out.println();
+		System.out.println();
+	}
+
+}
